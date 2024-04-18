49c0a411cdab15e841de29da465955c10ee8b4a5
Mirado <hei.mirado@gmail.com>
2024-04-18 16:24:42 +0300

    config pageable params exceptions

---
+++ src/main/java/com/journal/diaryko/model/BoundedPageSize.java
@@ -0,0 +1,20 @@
+package com.journal.diaryko.model;
+
+import com.journal.diaryko.model.exception.BadRequestException;
+import lombok.Getter;
+
+@Getter
+public class BoundedPageSize {
+    public static final int MAX_PAGE= 500;
+    private final int value;
+
+    public BoundedPageSize(int value){
+        if(value < 1){
+            throw new BadRequestException("Page size must be >= 1");
+        }
+        if(value > MAX_PAGE){
+            throw new BadRequestException("Page size must be < " + MAX_PAGE);
+        }
+        this.value = value;
+    }
+}
