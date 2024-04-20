74ca6531b9df29d3aa1b3575459853aaa7963f04
Mirado <hei.mirado@gmail.com>
2024-04-20 12:37:29 +0300

    add not found exception to journal service

--- src/main/java/com/journal/diaryko/service/JournalService.java
+++ src/main/java/com/journal/diaryko/service/JournalService.java
@@ -2,8 +2,11 @@ package com.journal.diaryko.service;
 
 import com.journal.diaryko.model.BoundedPageSize;
 import com.journal.diaryko.model.PageFromOne;
+import com.journal.diaryko.model.exception.NotFoundException;
 import com.journal.diaryko.repository.JournalRepository;
+import com.journal.diaryko.repository.UserRepository;
 import com.journal.diaryko.repository.model.Journal;
+import com.journal.diaryko.repository.model.User;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,9 +17,14 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class JournalService {
-    private final JournalRepository repository;
+
+    private final JournalRepository journalRepository;
+    private final UserRepository userRepository;
     public List<Journal> getJournalsByUserId(String userId, PageFromOne page, BoundedPageSize boundedPageSize){
+        User user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new NotFoundException("User with id " + userId + " not found"));
         Pageable pageable = PageRequest.of(page.getValue() - 1, boundedPageSize.getValue());
-        return repository.findAllByUserId(userId, pageable);
+        return journalRepository.findAllByUserId(user.getId(), pageable);
     }
 }
