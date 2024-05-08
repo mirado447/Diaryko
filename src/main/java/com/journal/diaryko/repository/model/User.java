aaecb35bf32c519bcd5de2d16015012e8d5aa443
Mirado <hei.mirado@gmail.com>
2024-05-08 13:03:23 +0300

    Chnage sex in type Sex enum

--- src/main/java/com/journal/diaryko/repository/model/User.java
+++ src/main/java/com/journal/diaryko/repository/model/User.java
@@ -1,12 +1,17 @@
 package com.journal.diaryko.repository.model;
 
+import com.journal.diaryko.model.enums.Sex;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 
 import java.time.LocalDate;
 @Data
@@ -22,6 +27,10 @@ public class User {
     private String lastname;
     private String email;
     private LocalDate birthdate;
-    private String sex ;
+
+    @Enumerated(EnumType.STRING) // The value in the column will be string type, not index (ex: 'M' not sex.M)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    private Sex sex;
+
     private String password;
 }
