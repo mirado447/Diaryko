50841ef8f78d872fd7ce04f8154ecd9132b108cf
Mirado <hei.mirado@gmail.com>
2024-05-08 13:05:16 +0300

    create enum sex type in postgres

--- src/main/resources/db/migration/V1_1__Create_user_table.sql
+++ src/main/resources/db/migration/V1_1__Create_user_table.sql
@@ -1,9 +1,18 @@
+do
+$$
+    begin
+            if not exists(select from pg_type where typname = 'sex') then
+    create type sex as enum ('M', 'F', 'OTHER');
+end if;
+    end
+$$;
+
 create table if not exists "user" (
     id varchar(100) primary key,
     firstname varchar(100),
     lastname varchar(100),
     email varchar(100),
     birthdate date,
-    sex varchar(20),
+    sex sex,
     password varchar(100)
     );
