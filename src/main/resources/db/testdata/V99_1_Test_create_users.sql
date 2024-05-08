d46284b8f854fc34e1a5c2f555af4206032432bf
Mirado <hei.mirado@gmail.com>
2024-05-08 13:07:26 +0300

    chore: change sex data name

--- src/main/resources/db/testdata/V99_1_Test_create_users.sql
+++ src/main/resources/db/testdata/V99_1_Test_create_users.sql
@@ -8,6 +8,6 @@ INSERT INTO "user" (
     password
 )
 VALUES
-    ('user_1', 'Ryan', 'Andria', 'test@gmail.com', '01-01-1995', 'Male', 'uQp7l4pzKuaaqCXjruhZw525pI23'),
-    ('user_2', 'Lila', 'Fenomanana', 'Lila@gmail.com', '15-05-1995', 'Female', 'uQgyurgbuei'),
-    ('user_3', 'Naivo', 'Lala', 'Naivo@gmail.com', '12-01-1999', 'Male', '112654ergv');
+    ('user_1', 'Ryan', 'Andria', 'test@gmail.com', '01-01-1995', 'M', 'uQp7l4pzKuaaqCXjruhZw525pI23'),
+    ('user_2', 'Lila', 'Fenomanana', 'Lila@gmail.com', '15-05-1995', 'F', 'uQgyurgbuei'),
+    ('user_3', 'Naivo', 'Lala', 'Naivo@gmail.com', '12-01-1999', 'OTHER', '112654ergv');
