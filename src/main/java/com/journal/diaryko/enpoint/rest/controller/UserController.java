7d307a8c7773c174e8b818001c40ff13d768039c
Mirado <hei.mirado@gmail.com>
2024-05-09 12:43:11 +0300

    use userMapper in userController

--- src/main/java/com/journal/diaryko/enpoint/rest/controller/UserController.java
+++ src/main/java/com/journal/diaryko/enpoint/rest/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.journal.diaryko.enpoint.rest.controller;
 
+import com.journal.diaryko.enpoint.mapper.UserMapper;
 import com.journal.diaryko.model.BoundedPageSize;
 import com.journal.diaryko.model.PageFromOne;
 import com.journal.diaryko.repository.model.User;
@@ -14,28 +15,32 @@ import java.util.List;
 public class UserController {
 
     private final UserService service;
+    private final UserMapper userMapper;
 
     @GetMapping("/users")
-    public List<User> getUsers(
+    public List<com.journal.diaryko.endpoint.rest.model.User> getUsers(
             @RequestParam int page,
             @RequestParam(value = "page_size") int pageSize){  //value = "page_size" is the name of query params in url
         PageFromOne pageFromOne = new PageFromOne(page);
         BoundedPageSize boundedPageSize = new BoundedPageSize(pageSize);
-        return service.getAllUser(pageFromOne, boundedPageSize);
+        return service.getAllUser(pageFromOne, boundedPageSize)
+                .stream()
+                .map(userMapper::toRest)
+                .toList();
     }
     @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable String id){
-        return service.getUserById(id);
+    public com.journal.diaryko.endpoint.rest.model.User getUserById(@PathVariable String id){
+        return userMapper.toRest(service.getUserById(id));
     }
 
     @PutMapping("/users/{id}")
-    public User crupdateUser(
+    public com.journal.diaryko.endpoint.rest.model.User crupdateUser(
             @PathVariable String id,
-            @RequestBody User user){
-        return service.saveUser(id, user);
+            @RequestBody com.journal.diaryko.endpoint.rest.model.User user){
+        return userMapper.toRest(service.saveUser(id, userMapper.toDomain(user)));
     }
     @DeleteMapping("/users/{id}")
-    public User deleteUser(@PathVariable String id){
-        return service.deleteUser(id);
+    public com.journal.diaryko.endpoint.rest.model.User deleteUser(@PathVariable String id){
+        return userMapper.toRest(service.deleteUser(id));
     }
 }
