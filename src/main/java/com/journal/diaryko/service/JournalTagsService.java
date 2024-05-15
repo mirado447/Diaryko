7434b25c019b54bd8f47718f16772bc7464ee5d3
Mirado <hei.mirado@gmail.com>
2024-05-15 11:54:07 +0300

    add exception in methode AddJournalTags and create deleteJournalTags methode

--- src/main/java/com/journal/diaryko/service/JournalTagsService.java
+++ src/main/java/com/journal/diaryko/service/JournalTagsService.java
@@ -1,5 +1,6 @@
 package com.journal.diaryko.service;
 
+import com.journal.diaryko.model.exception.ResourceAlreadyExistsException;
 import com.journal.diaryko.repository.JournalTagsRepository;
 import com.journal.diaryko.repository.model.Journal;
 import com.journal.diaryko.repository.model.JournalTags;
@@ -20,15 +21,28 @@ public class JournalTagsService {
         Journal journal = journalService.getJournalById(jid);
         return repository.findAllByJournalId(journal.getId());
     }
-    public Tag AddJournalTags(String jid,String tid){      //TODO: chose the appropriate return type
+    public Tag AddJournalTags(String jid,String tid){
         Journal journal = journalService.getJournalById(jid);
         Tag tag = tagService.getById(tid);
+        JournalTags existingJournalTags = repository.findByJournalIdAndTagId(jid, tid);
+
+        if(existingJournalTags != null){
+            throw new ResourceAlreadyExistsException("Tag: " + tag.getTag_name() + " is already added to this journal");
+        }
+
         JournalTags journalTags = JournalTags.builder()
                 .id(jid + "_" + tag.getId())    //TODO: change the generation id methode
                 .tag(tag)
                 .journal(journal)
                 .build();
         repository.save(journalTags);
+        return tag;
+    }
+
+    public Tag DeleteJournalTags(String jid, String tid){
+        JournalTags journalTags = repository.findByJournalIdAndTagId(jid, tid);
+        Tag tag = tagService.getById(tid);
+        repository.delete(journalTags);
         return tag;
     }
 }
