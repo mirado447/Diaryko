c72cc076269fd51997566a7256db80e4cdcdedcb
Mirado <hei.mirado@gmail.com>
2024-05-15 15:13:11 +0300

    remove * import

--- src/main/java/com/journal/diaryko/enpoint/rest/controller/PhotoController.java
+++ src/main/java/com/journal/diaryko/enpoint/rest/controller/PhotoController.java
@@ -5,7 +5,13 @@ import com.journal.diaryko.model.BoundedPageSize;
 import com.journal.diaryko.model.PageFromOne;
 import com.journal.diaryko.service.PhotoService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
