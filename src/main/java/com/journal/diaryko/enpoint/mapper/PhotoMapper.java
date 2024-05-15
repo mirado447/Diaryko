bfba1933b1872b3f1335eff4512f3e60f64ea99b
Mirado <hei.mirado@gmail.com>
2024-05-15 13:19:49 +0300

    add photo mapper

---
+++ src/main/java/com/journal/diaryko/enpoint/mapper/PhotoMapper.java
@@ -0,0 +1,28 @@
+package com.journal.diaryko.enpoint.mapper;
+
+import com.journal.diaryko.repository.model.Photo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class PhotoMapper {
+    public com.journal.diaryko.endpoint.rest.model.Photo toRest(Photo domain){
+        return new com.journal.diaryko.endpoint.rest.model.Photo()
+                .id(domain.getId())
+                .filename(domain.getName())
+                .typeMime(domain.getType_mime())
+                .fileSize(domain.getFile_size())
+                .filepath(domain.getFilepath());
+    }
+
+    public Photo toDomain(com.journal.diaryko.endpoint.rest.model.Photo rest){
+        return Photo.builder()
+                .id(rest.getId())
+                .name(rest.getFilename())
+                .type_mime(rest.getTypeMime())
+                .file_size(rest.getFileSize())
+                .filepath(rest.getFilepath())
+                .build();
+    }
+}
